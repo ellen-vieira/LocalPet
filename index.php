9bf92445a75df3cd79848af122e63e6bc80960b8
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2023-03-08 00:01:41 -0300

    novo

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
+<!--
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- TITULO -->
-    <title>Local Pet</title>
+<!--     <title>Local Pet</title> -->
     <!-- LINK BOOTSTRAP -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+<!--     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet"> -->
     <!-- ICON -->
-    <link rel="icon" type="image/png" href="images/favicon.png"/>
+<!--     <link rel="icon" type="image/png" href="images/favicon.png"/> -->
     <!-- DA ONDE VEM A COR DA HEADER -->
-    <link rel="stylesheet" href="css/bootstrap.css">
+<!--     <link rel="stylesheet" href="css/bootstrap.css"> -->
     <!-- ESTILO HEADER -->
-    <style>
+<!--     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -54,73 +55,73 @@
       }
     </style>
 </head>
-<body>
+<body> -->
 <!-- LINK DOS ICONS -->
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+<!-- <svg xmlns="http://www.w3.org/2000/svg" style="display: none;"> -->
   <!-- HOME -->
-  <symbol id="home" viewBox="0 0 16 16">
+<!--   <symbol id="home" viewBox="0 0 16 16">
     <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z"/>
-  </symbol>
+  </symbol> -->
   <!-- SOBRE -->
-  <symbol id="speedometer2" viewBox="0 0 16 16">
+<!--   <symbol id="speedometer2" viewBox="0 0 16 16">
     <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm5 4a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-  </symbol>
+  </symbol> -->
   <!-- ONGS -->
-  <symbol id="table" viewBox="0 0 16 16">
+<!--   <symbol id="table" viewBox="0 0 16 16">
     <path d="M7.207 1a1 1 0 0 0-1.414 0L.146 6.646a.5.5 0 0 0 .708.708L1 7.207V12.5A1.5 1.5 0 0 0 2.5 14h.55a2.51 2.51 0 0 1-.05-.5V9.415a1.5 1.5 0 0 1-.56-2.475l5.353-5.354L7.207 1Z"/>
     <path d="M8.793 2a1 1 0 0 1 1.414 0L12 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l1.854 1.853a.5.5 0 0 1-.708.708L15 8.207V13.5a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 4 13.5V8.207l-.146.147a.5.5 0 1 1-.708-.708L8.793 2Z"/>
-  </symbol>
+  </symbol> -->
   <!-- ADMIN -->
-  <symbol id="people-circle" viewBox="0 0 16 16">
+<!--   <symbol id="people-circle" viewBox="0 0 16 16">
     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
-  </symbol>
+  </symbol> -->
   <!-- ADOÇÃO -->
-  <symbol id="grid" viewBox="0 0 16 16">
+<!--   <symbol id="grid" viewBox="0 0 16 16">
     <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4h-8.5ZM8.5 4h6l.5.667V5H1v-.333L1.5 4h6V1h1v3ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
   </symbol>
 </svg>
-<header>
+<header> -->
     <!-- COR DA HEADER -->
-    <div class="px-3 py-2 text-bg-danger">
+<!--     <div class="px-3 py-2 text-bg-danger"> -->
       <!-- CONTAINER HEADER -->
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+<!--       <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"> -->
         <!-- LOGO -->
-        <img src="images/localpet.png" alt="Local Pet" height="100px" width="100px">
+<!--         <img src="images/localpet.png" alt="Local Pet" height="100px" width="100px">
           <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
             <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
           </a>
-          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small"> -->
             <!-- HOME -->
-            <li>
+<!--             <li>
               <a href="#" class="nav-link text-white">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"/></svg>
                 Home
               </a>
-            </li>
+            </li> -->
             <!-- SOBRE -->
-            <li>
+<!--             <li>
               <a href="#" class="nav-link text-white">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"/></svg>
                 Sobre
               </a>
-            </li>
+            </li> -->
             <!-- ONGS -->
-            <li>
+<!--             <li>
               <a href="#" class="nav-link text-white">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#table"/></svg>
                 ONGs
               </a>
-            </li>
+            </li> -->
             <!-- ADOÇÃO -->
-            <li>
+<!--             <li>
               <a href="#" class="nav-link text-white">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"/></svg>
                 Adoção
               </a>
-            </li>
+            </li> -->
             <!-- ADMIN -->
-            <li>
+<!--             <li>
               <a href="#" class="nav-link text-white">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
                 <span class="glyphicon glyphicon-user"></span>
@@ -130,25 +131,69 @@
           </ul>
         </div>
       </div>
-    </div>
+    </div> -->
     <!-- PROCURAR -->
-    <div class="px-3 py-2 border-bottom mb-3">
+<!--     <div class="px-3 py-2 border-bottom mb-3">
       <div class="container d-flex flex-wrap justify-content-center">
         <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
           <input type="search" class="form-control" placeholder="Procurar..." aria-label="Search">
-        </form>
-        <!-- LOGIN -->
-        <div class="text-end">
+        </form> -->
+        <!--LOGIN -->
+<!--         <div class="text-end">
           <button type="button" class="btn btn-light text-dark me-2">Login</button>
           <button type="button" class="btn btn-danger">Sign-up</button>
         </div>
       </div>
     </div>
-  </header>
+  </header> 
 
-</main>
-    <!-- SCRIPT -->
+</main> -->
+    <!-- SCRIPT 
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.lordicon.com/ritcuqlt.js%22%3E"></script>
 </body>
+</html>-->
+
+<!-- NOVO -->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- TITULO -->
+    <title>Local Pet</title>
+    <!-- LINK BOOTSTRAP -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- ICON -->
+    <link rel="icon" type="image/png" href="images/favicon.png"/>
+  
+</head>
+<body>
+
+ <!-- Área de menu -->
+ <?php include 'home.php'; ?>
+    <a name="home">&nbsp;</a>
+    <main class="container">
+
+</body>
+
+<!-- Link arquivos Bootstrap js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+    $(document).on('ready', function() {
+        $(".regular").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        });
+    });
+</script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </html>
