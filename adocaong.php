cd518c7e39d538a29b62f072744657483f267057
PALOMA DE CASTRO LEITE <paloma.dcastroleite@gmail.com>
2023-03-14 09:20:41 -0300

    mudei

--- adocaong.php
+++ adocaong.php
@@ -1,132 +1,90 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style1.css">
-
+    
     <title>Formulário de Adoção - ONG</title>
 </head>
 <body>
-    <!-- DIV IMAGEM -->
-    <div class="container">
-        <div class="form-image">
-            <img src="images/adocaong.png" alt="">
+<div class="container px-5 my-5">
+    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+        <div class="mb-3">
+            <label class="form-label" for="nomeDoAnimal">Nome do animal</label>
+            <input class="form-control" id="nomeDoAnimal" type="text" placeholder="Digite o nome do animal" data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="nomeDoAnimal:required">Nome do animal é obrigatório.</div>
         </div>
-        <div class="form">
-            <form action="#">
-                <div class="form-header">
-                    <div class="title">
-                        <h1>Formulário de Adoção - ONG</h1>
-                    </div>
-                    <!-- BOTÃO -->
-                    <div class="back-button ms-4">
-                        <button><a href="home.php">Voltar</a></button>
-                    </div>
-                </div>
-
-                <!-- FORMULÁRIO -->
-
-                   <!-- ESPÉCIES -->
-                <div class="species-inputs">
-                    <div class="species-title">
-                        <h5>Espécie</h5>
-                    </div>
-                    <div class="species-group">
-                        <div class="species-input">
-                            <input id="female" type="radio" name="species">
-                            <label class="me-2" for="female">Gato</label>
-                        </div>
-                        <div class="species-input">
-                            <input id="male" type="radio" name="species">
-                            <label for="male">Cachorro</label>
-                        </div>
-                    </div>
-                    
-                    <div class="species-title mt-2">
-                        <h5>Porte</h5>
-                    </div>
-                    <div class="species-group">
-                        <div class="species-input">
-                            <input id="small" type="radio" name="species">
-                            <label class="me-2" for="small">Pequeno</label>
-                        </div>
-                        <div class="species-input">
-                            <input id="medium" type="radio" name="species">
-                            <label class="me-2" for="medium">Médio</label>
-                        </div>
-                        <div class="species-input">
-                            <input id="big" type="radio" name="species">
-                            <label for="big">Grande</label>
-                        </div>
-                    </div>
-
-                    <div class="species-title">
-                        <h5>Sexo</h5>
-                    </div>
-                    <div class="species-group">
-                        <div class="species-input">
-                            <input id="male" type="radio" name="species">
-                            <label class="me-2" for="male">Macho</label>
-                        </div>
-                        <div class="species-input">
-                            <input id="female" type="radio" name="species">
-                            <label for="female">Fêmea</label>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- CAIXA DE TEXTO -->
-                   <div class="input-group">
-                    <div class="input-box">
-                        <label for="age"> Nome</label>
-                        <input id="age" type="text" name="age" placeholder="Digite o nome do animal." required>
-                    </div> 
-                 </div>
-               
-                    <!-- IDADE  -->
-                <!-- <div class="input-group">
-                    <div class="input-box">
-                        <label for="age"> Idade</label>
-                        <input id="age" type="text" name="age" placeholder="Digite a idade aproximada." required>
-                    </div> -->
-                     <!-- CARACTERISTICAS -->
-                    <!-- <div class="input-box">
-                        <label for="character">Características fisícas</label>
-                        <input id="character" type="text" name="character" placeholder="Digite a aparência do animal" required>
-                    </div> -->
-                    <!-- SAÚDE  -->
-                    <!-- <div class="input-box">
-                        <label for="health"> Possui problema de saúde e/ou deficiência?</label>
-                        <input id="health" type="text" name="health" placeholder="Digite quais se houver." required>
-                    </div> -->
-                    <!-- MEDICAÇÃO -->
-                     <!-- <div class="input-box">
-                        <label for="medication"> Toma algum tipo de medicação?</label>
-                        <input id="medication" type="text" name="medication" placeholder="Digite quais se houver." required>
-                    </div> -->
-                      <!-- VACINAS  -->
-                     <!-- <div class="input-box">
-                        <label for="vaccine">Vacinas</label>
-                        <input id="vaccine" type="text" name="vaccine" placeholder="Digite quais se houver." required>
-                    </div> -->
-                      <!-- COMPORTAMENTO  -->
-                     <!-- <div class="input-box">
-                        <label for="behavior">Comportamento (hábitos, personalidade, etc)</label>
-                        <input id="behavior" type="text" name="behavior" placeholder="Digite aqui." required>
-                    </div>
-                </div> -->
-
-                <!-- BOTÃO -->
-                <div class="continue-button ms-2">
-                    <button><a href="#">Continuar</a> </button>
-                </div>
-            </form>
+        <div class="mb-3">
+            <label class="form-label" for="racaDoAnimal">Raça do animal</label>
+            <input class="form-control" id="racaDoAnimal" type="text" placeholder="Digite a raça do animal" data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="racaDoAnimal:required">Raça do animal é obrigatório.</div>
         </div>
-    </div>
+        <div class="mb-3">
+            <label class="form-label d-block">Sexo</label>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" id="macho" type="radio" name="sexo" data-sb-validations="required" />
+                <label class="form-check-label" for="macho">Macho</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" id="femea" type="radio" name="sexo" data-sb-validations="required" />
+                <label class="form-check-label" for="femea">Fêmea</label>
+            </div>
+            <div class="invalid-feedback" data-sb-feedback="sexo:required">Preencha uma das opções.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label d-block">Porte</label>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" id="pequeno" type="radio" name="porte" data-sb-validations="required" />
+                <label class="form-check-label" for="pequeno">Pequeno</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" id="medio" type="radio" name="porte" data-sb-validations="required" />
+                <label class="form-check-label" for="medio">Médio</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" id="grande" type="radio" name="porte" data-sb-validations="required" />
+                <label class="form-check-label" for="grande">Grande</label>
+            </div>
+            <div class="invalid-feedback" data-sb-feedback="porte:required">Preencha uma das opções.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="idadeDoAnimal">Idade do animal</label>
+            <input class="form-control" id="idadeDoAnimal" type="text" placeholder="Idade do animal" data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="idadeDoAnimal:required">Idade do animal é obrigatório.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="caracteristicasFisicas">Características físicas</label>
+            <input class="form-control" id="caracteristicasFisicas" type="text" placeholder="Cor do pelo e olhos, manchas, etc. " data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="caracteristicasFisicas:required">Características físicas é obrigatório.</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="possuiProblemaDeSaudeEOuDeficiencia"> Possui problema de saúde e/ou deficiência?</label>
+            <input class="form-control" id="possuiProblemaDeSaudeEOuDeficiencia" type="text" placeholder=" Possui problema de saúde e/ou deficiência?" data-sb-validations="" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="tomaAlgumTipoDeMedicacao">Toma algum tipo de medicação?</label>
+            <input class="form-control" id="tomaAlgumTipoDeMedicacao" type="text" placeholder="Toma algum tipo de medicação?" data-sb-validations="" />
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="vacinasRecebidas">Vacinas recebidas</label>
+            <input class="form-control" id="vacinasRecebidas" type="text" placeholder="Vacinas  recebidas" data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="vacinasRecebidas:required">Vacinas recebidas é obrigatório</div>
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="comportamentoHabitosPersonalidadeEtc">Comportamento</label>
+            <input class="form-control" id="comportamentoHabitosPersonalidadeEtc" type="text" placeholder="Hábitos, personalidade, etc)" data-sb-validations="required" />
+            <div class="invalid-feedback" data-sb-feedback="comportamentoHabitosPersonalidadeEtc:required">Comportamento é obrigatório.</div>
+        </div>
+       
+        <div class="d-grid">
+            <button class="btn btn-danger btn-lg disabled" id="submitButton" type="submit">Enviar</button>
+        </div>
+    </form>
+</div>
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+              
+          
 </body>
+</html> 
 
-</html>
