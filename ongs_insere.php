811262f49b46c8255666269e8b2e7861102ba9c4
EllenVieira1 <vieiraellen755@gmail.com>
2023-03-14 18:07:46 -0300

    Mudanças

---
+++ ongs_insere.php
@@ -0,0 +1,94 @@
+<?php
+// INCLUIR  O ARQUIVO E FAZER A CONEXÃO
+include('conn/connect.php');
+
+if ($_POST) {
+
+    // GUARDA O NOME DA IMAGEM NO BANCO E O ARQUIVO NO DIRETÓRIO
+    if (isset($_POST['enviar'])) {
+        $nome_img   =   $_FILES['imagem_ong']['name'];
+        $tmp_img    =   $_FILES['imagem_ong']['tmp_name'];
+        $dir_img    =   "../images/" . $nome_img;
+        move_uploaded_file($tmp_img, $dir_img);
+    };
+
+
+    // RECEBER OS DADOS DO FORMULÁRIO
+    // ORGANIZAR OS CAMPOS NA MESMA ORDEM
+    $nome = $_POST['nome'];
+    $cnpj = $_POST['cnpj'];
+    $cpf = $_POST['cpf'];
+    $descricao = $_POST['descricao'];
+    $imagem_ong = $_FILES['imagem_ong']['name'];
+
+    // CONSULTA SQL PARA INSERÇÃO DE DADOS
+    $insertSQL = "INSERT INTO ongs
+                    ( nome, cnpj, cpf, descricao, imagem_ong)
+                    VALUES
+                    ('$nome', '$cnpj', '$cpf', '$descricao', '$imagem_ong')
+                    ";
+    $resultado = $conn->query($insertSQL);
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar ONGs</title>
+    <!-- LINKS BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<?php include 'header.php'; ?>
+<div>
+    <section class="d-flex justify-content-center align-items-center">
+        <div class="card shadow col-xs-12 col-sm-6 col-md-6 col-lg-4   p-4">
+            <div class="mb-4 d-flex justify-content-start align-items-center">
+                <h4>Cadastrar ONGs</h4>
+            </div>
+            <div class="mb-1">
+                <form id="cadastro">
+                    <!-- NOME -->
+                    <div class="mb-4">
+                        <div>
+                            <label for="nome">Nome:</label>
+                            <input type="text" class="form-control" name="nome" id="nome" required>
+                        </div>
+                    </div>
+                    <!-- CNPJ -->
+                    <div class="mb-4">
+                        <label for="cnpj">CNPJ:</label>
+                        <input type="cnpj" class="form-control" name="cnpj" id="cnpj" required>
+                    </div>
+                    <!-- CPF -->
+                    <div class="mb-4">
+                        <label for="cpf">CPF:</label>
+                        <input type="cpf" class="form-control" name="cpf" id="cpf" required>
+                    </div>
+                    <!-- DESCRIÇÃO -->
+                    <div class="mb-4">
+                        <label for="descricao">Descrição:</label>
+                        <textarea name="descricao" id="descricao" class="form-control"></textarea>
+                    </div>
+                    <!-- IMAGEM -->
+                    <div class="mb-4">
+                        <label for="imagem" class="form-label">Imagem da sua ONG:</label>
+                        <input class="form-control" type="file" id="imagem">
+                    </div>
+                    <!-- BOTÃO -->
+                    <div class="mb-2">
+                        <button type="submit" id="enviar" class="col-12 btn btn-danger justify-content-between">
+                            <span>Enviar</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</div>
+
+</html>
--- usuarios_insere.php
+++ usuarios_insere.php
@@ -1,76 +1,72 @@
 <?php
-// Incluir o arquivo e fazer a conexão
+// INCLUIR  O ARQUIVO E FAZER A CONEXÃO
 include('conn/connect.php');
 
 if ($_POST) {
 
-    // Receber os dados do formulário
-    // Organizar os campos na mesma ordem
+    // RECEBER OS DADOS DO FORMULÁRIO
+    // ORGANIZAR OS CAMPOS NA MESMA ORDEM
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    // Consulta SQL para inserção de dados
+    // CONSULTA SQL PARA INSERÇÃO DE DADOS
     $insertSQL = "INSERT INTO usuarios
                     ( nome, email, senha)
                     VALUES
                     ('$nome', '$email' , '$senha')
                     ";
+                    var_dump($insertSQL);
     $resultado = $conn->query($insertSQL);
 }
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <!-- LINKS BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Cadastro</title>
 </head>
-
-<body>
-    <!-- DIV IMAGEM -->
-    <div class="container">
-        <div class="form-image">
-            <img src="images/Usuario.png" alt="">
+<?php include 'header.php'; ?>
+<div>
+    <section class="d-flex justify-content-center align-items-center">
+        <div class="card shadow col-xs-12 col-sm-6 col-md-6 col-lg-4   p-4">
+            <div class="mb-4 d-flex justify-content-start align-items-center">
+                <h4>Cadastre-se</h4>
+            </div>
+            <div class="container">
+                <form id="cadastro">
+                    <!-- NOME -->
+                    <div class="mb-4">
+                        <div>
+                            <label for="nome">Nome:</label>
+                            <input id="nome" type="text" name="nome"  class="form-control" placeholder="Digite seu nome..." required>
+                        </div>
+                    </div>
+                    <!-- EMAIL -->
+                    <div class="mb-4">
+                        <label for="email">Email:</label>
+                        <input id="email" type="email" name="email" class="form-control" placeholder="Digite seu E-Mail..." required>
+                    </div>
+                    <!-- SENHA -->
+                    <div class="mb-4">
+                        <label for="senha">Senha:</label>
+                        <input id="senha" type="password" name="senha" class="form-control"   placeholder="Digite sua senha..." required>
+                    </div>
+                    <!-- BOTÃO -->
+                    <div class="mb-2">
+                        <button type="submit" id="enviar" class="col-12 btn btn-danger justify-content-between ">
+                            <span>Enviar</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="form">
-            <form action="#" method="POST">
-                <div class="form-header">
-                    <!-- TITULO -->
-                    <div class="title">
-                        <h1>Cadastro</h1>
-                    </div>
-                    <!-- BOTÃO VOLTAR -->
-                    <div class="login-button">
-                        <button><a href="home.php">Voltar</a></button>
-                    </div>
-                </div>
-                <!-- INPUTS -->
-                <div class="input-group">
-                    <div class="input-box">
-                        <label for="Nome">Nome</label>
-                        <input id="nome" type="text" name="nome" placeholder="Digite seu Nome" required>
-                    </div>
-                    <div class="input-box">
-                        <label for="email">E-mail</label>
-                        <input id="email" type="email" name="email" placeholder="Digite seu e-mail" required>
-                    </div>
-                    <div class="input-box">
-                        <label for="senha">Senha</label>
-                        <input id="senha" type="password" name="senha" placeholder="Digite sua senha" required>
-                    </div>
-                </div>
-
-                <div class="continue-button">
-                    <button type="submit" class="btn btn-danger btn-block">Entrar</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-</body>
-
+    </section>
+</div>
 </html>
